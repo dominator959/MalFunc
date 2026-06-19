dcf9a4a56e07b55bf9db88ca85a02a954952bcab
faizantoheed456 <faizantoheed456@gmail.com>
2026-06-19 19:01:12 +0500

    Finalizing the library

--- malfunc/__init__.py
+++ malfunc/__init__.py
@@ -1,5 +1,5 @@
-from .ops import get_number, get_sum, get_mul
-from .utils import get_indices, vectorize_mul
+from .ops import get_number, get_sum, get_mul, get_indices, vectorize_sum
+from .utils import vectorize_mul, get_unique_order_list, get_flat, get_dtypes
 
 __all__ = [
     "get_number",
